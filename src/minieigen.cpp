cef9dc0f38e41cc94bfcc753bfee3f27f78683f6
Vincent Samy <vincent.samy@lirmm.fr>
2016-10-21 16:41:59 +0200

    Operationnal cmake installer

--- src/minieigen.cpp
+++ src/minieigen.cpp
@@ -11,39 +11,30 @@
 The code is split to live in several files to reduce the amount of RAM necessary for compilation -- see http://www.boost.org/doc/libs/1_52_0/libs/python/doc/v2/faq.html#slow_compilation for the suggestion of this technique.
 */
 
-#include"common.hpp"
-#include"expose.hpp"
+#include "common.hpp"
+#include "expose.hpp"
 
-BOOST_PYTHON_MODULE(minieigen){
-	py::scope().attr("__doc__")="miniEigen is wrapper for a small part of the `Eigen <http://eigen.tuxfamily.org>`_ library. Refer to its documentation for details. All classes in this module support pickling.";
+BOOST_PYTHON_MODULE(_minieigen)
+{
+    py::scope().attr("__doc__") = "miniEigen is wrapper for a small part of the `Eigen <http://eigen.tuxfamily.org>`_ library. Refer to its documentation for details. All classes in this module support pickling.";
 
-	py::docstring_options docopt;
-	docopt.enable_all();
-	docopt.disable_cpp_signatures();
+    py::docstring_options docopt;
+    docopt.enable_all();
+    docopt.disable_cpp_signatures();
 
+    expose_converters(); // in expose-converters.cpp
 
+    expose_vectors();
+    expose_matrices(); // must come after vectors
+    expose_complex();
+    expose_quaternion();
+    expose_boxes();
 
-	expose_converters(); // in expose-converters.cpp
+    py::def("float2str", &doubleToShortest, (py::arg("f"), py::arg("pad") = 0), "Return the shortest string representation of *f* which will is equal to *f* when converted back to float. This function is only useful in Python prior to 3.0; starting from that version, standard string conversion does just that.");
 
-	expose_vectors();
-	expose_matrices(); // must come after vectors
-	expose_complex();
-	expose_quaternion();
-	expose_boxes();
-
-	py::def("float2str",&doubleToShortest,(py::arg("f"),py::arg("pad")=0),"Return the shortest string representation of *f* which will is equal to *f* when converted back to float. This function is only useful in Python prior to 3.0; starting from that version, standard string conversion does just that.");
-
-	#ifdef EIGEN_DONT_ALIGN
-		py::scope().attr("vectorize")=false;
-	#else
-		py::scope().attr("vectorize")=true;
-	#endif
+#ifdef EIGEN_DONT_ALIGN
+    py::scope().attr("vectorize") = false;
+#else
+    py::scope().attr("vectorize") = true;
+#endif
 };
-
-
-
-
-
-
-
-
